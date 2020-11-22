59171406c51becf29e2122753d6597985bb6d1f3
YuryGrivickiy <griv1993@gmail.com>
2020-11-22 17:58:52 +0300

    Updates

---
+++ precache-manifest.57eb36aa16eb14aa2a6ffb0d16f4ac62.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ddea6a182beabb1ffdae40561bf994",
+    "url": "/AD1N1993/YourDesigner/index.html"
+  },
+  {
+    "revision": "b77dc64bde967ce317ec",
+    "url": "/AD1N1993/YourDesigner/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "bd0ff269c6ba4988bae3",
+    "url": "/AD1N1993/YourDesigner/static/css/main.ac912a1d.chunk.css"
+  },
+  {
+    "revision": "b77dc64bde967ce317ec",
+    "url": "/AD1N1993/YourDesigner/static/js/2.dff1a729.chunk.js"
+  },
+  {
+    "revision": "8da57ccee1690fd1d0790b5fcb8f1bad",
+    "url": "/AD1N1993/YourDesigner/static/js/2.dff1a729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0ff269c6ba4988bae3",
+    "url": "/AD1N1993/YourDesigner/static/js/main.59126ed7.chunk.js"
+  },
+  {
+    "revision": "1ca1dda71172860886fd",
+    "url": "/AD1N1993/YourDesigner/static/js/runtime-main.7b0b21ef.js"
+  },
+  {
+    "revision": "3e08e5b8feb25df5948a7b7bef755a97",
+    "url": "/AD1N1993/YourDesigner/static/media/LamaBig.3e08e5b8.jpg"
+  },
+  {
+    "revision": "586273d2575af03b2bfc6c6c54729756",
+    "url": "/AD1N1993/YourDesigner/static/media/RP.586273d2.png"
+  },
+  {
+    "revision": "e7245dfc607b1f2d3969c5d293246390",
+    "url": "/AD1N1993/YourDesigner/static/media/adaptiveDesign.e7245dfc.svg"
+  },
+  {
+    "revision": "bd93f0f8c783e33ca63975b3a5574f63",
+    "url": "/AD1N1993/YourDesigner/static/media/coffe.bd93f0f8.jpg"
+  },
+  {
+    "revision": "b9d04fcaf5673e3c874b5d54c6ccedf5",
+    "url": "/AD1N1993/YourDesigner/static/media/colab.b9d04fca.png"
+  },
+  {
+    "revision": "5a258a35b42fcf03cc43a688daf5a543",
+    "url": "/AD1N1993/YourDesigner/static/media/communication.5a258a35.svg"
+  },
+  {
+    "revision": "8ec020ca364ba5e3c6a01d3de652ce08",
+    "url": "/AD1N1993/YourDesigner/static/media/designTools.8ec020ca.svg"
+  },
+  {
+    "revision": "e0a46afc283e5d10e84def13c51e8ce2",
+    "url": "/AD1N1993/YourDesigner/static/media/garlicBig.e0a46afc.jpg"
+  },
+  {
+    "revision": "b6c57b81aa990bb66849088e0f21c646",
+    "url": "/AD1N1993/YourDesigner/static/media/graficDesign.b6c57b81.svg"
+  },
+  {
+    "revision": "41ea6268564a3212c7fc25e5d357db8c",
+    "url": "/AD1N1993/YourDesigner/static/media/lama325.41ea6268.jpg"
+  },
+  {
+    "revision": "3b8cf3a6e83b2a2b72621dbc1189f08a",
+    "url": "/AD1N1993/YourDesigner/static/media/mainScreen.3b8cf3a6.jpg"
+  },
+  {
+    "revision": "56ddf2ffc644e6dfbb740356ebb7186f",
+    "url": "/AD1N1993/YourDesigner/static/media/me.56ddf2ff.jpg"
+  },
+  {
+    "revision": "756dedbaad320f069bf735c426ca0420",
+    "url": "/AD1N1993/YourDesigner/static/media/mobApp.756dedba.png"
+  },
+  {
+    "revision": "b49000c0d9828fd683319263717f6e73",
+    "url": "/AD1N1993/YourDesigner/static/media/pizza.b49000c0.jpg"
+  },
+  {
+    "revision": "35925a8bd00c5132d92940eb440be0de",
+    "url": "/AD1N1993/YourDesigner/static/media/skyscraper.35925a8b.png"
+  },
+  {
+    "revision": "efcd4bd4641e25d18fcdb011bec1abd0",
+    "url": "/AD1N1993/YourDesigner/static/media/thank.efcd4bd4.png"
+  },
+  {
+    "revision": "90ea1cf3871488ebd9e32c8fc7c5b1f7",
+    "url": "/AD1N1993/YourDesigner/static/media/timeManagement.90ea1cf3.svg"
+  },
+  {
+    "revision": "96b0001089538767221e98c769a08a81",
+    "url": "/AD1N1993/YourDesigner/static/media/uxuiDesign.96b00010.svg"
+  }
+]);
